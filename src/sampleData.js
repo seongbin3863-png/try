7783180a219350a81927559d97d7c4ace3499663
seongbin3863-png <seongbin3863@gmail.com>
2026-05-27 02:17:47 +0900

    Update sampleData.js

--- src/sampleData.js
+++ src/sampleData.js
@@ -353,4 +353,44 @@ export const ATMOSPHERE_TAGS_POOL = [
   '예약 필수',
   '동네 축덕 아지트',
   '맥덕들 추천'
-];
+],
+```js
+{
+  id: 1,
+  name: "신촌 낭만오지",
+  address: "서울 마포구 고산길 17",
+  screenType: "빔프로젝터",
+  naverMapUrl: "https://naver.me/5r9z7UxJ"
+},
+
+{
+  id: 2,
+  name: "신촌 썬더치킨",
+  address: "서울 서대문구 연세로11길 20 1층",
+  screenType: "빔프로젝터",
+  naverMapUrl: "https://naver.me/FTXDlTC9"
+},
+
+{
+  id: 3,
+  name: "신촌 뉴타운",
+  address: "서울 서대문구 연세로12길 27",
+  screenType: "TV 다수",
+  naverMapUrl: "https://naver.me/IGJyfK7B"
+},
+
+{
+  id: 4,
+  name: "홍대 만선호프",
+  address: "서울 마포구 와우산로17길 15 1층, 2층",
+  screenType: "빔프로젝터",
+  naverMapUrl: "https://naver.me/xq3drtjR"
+},
+
+{
+  id: 5,
+  name: "신촌 오퍼스",
+  address: "서울 서대문구 연세로7길 18 2층",
+  screenType: "빔프로젝터",
+  naverMapUrl: "https://naver.me/Gipd4Lqn"
+}
